a0e89f92243e7cfda5964d9829057a571b41b703
Antoine GIRARD <sapk@sapk.fr>
2016-12-01 15:49:17 +0100

    ..

--- 3_1-petit_shell.c
+++ 3_1-petit_shell.c
@@ -12,32 +12,43 @@
 #define TABSIZE 512
 
 int main() {
-  int pid, longueur;
-  char tab[TABSIZE], *s;
+    int pid, longueur;
+    char tab[TABSIZE], *s;
 
-  for (;;) {
-    fputs("petit_shell...> ", stdout);	/* Affichage d'un prompt */
-    s = fgets(tab, TABSIZE, stdin);
+    for (;;) {
+        fputs("petit_shell...> ", stdout); /* Affichage d'un prompt */
+        s = fgets(tab, TABSIZE, stdin);
 
-    if (s == NULL) {
-      fprintf(stderr, "Fin du Shell\n");
-      exit(EXIT_SUCCESS);
+        if (s == NULL) {
+            fprintf(stderr, "Fin du Shell\n");
+            exit(EXIT_SUCCESS);
+        }
+
+        longueur = strlen(s);
+        tab[longueur - 1] = '\0';
+
+        pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "Erreur during forking\n");
+            exit(EXIT_SUCCESS);
+        } else if (pid == 0) { //Child
+            int r = execl(s,"");
+            fprintf(stderr, "Erreur dans le exec %d\n", r);
+        } else { //Parent
+            //fprintf(stderr, "Erreur dans le exec\n")
+            wait(NULL);
+        }
+
+        /* Actions:
+         * 
+         * Si dans pere alors
+         *   wait(NULL);
+         * sinon alors
+         *   execution de la commande recuperee dans tab;
+         *   message d'erreur: fprintf(stderr, "Erreur dans le exec\n")
+         * fin si
+         */
+
     }
-
-    longueur = strlen(s);
-    tab[longueur - 1] = '\0';
-
-    pid = fork();
-
-    /* Actions:
-     * 
-     * Si dans pere alors
-     *   wait(NULL);
-     * sinon alors
-     *   execution de la commande recuperee dans tab;
-     *   message d'erreur: fprintf(stderr, "Erreur dans le exec\n")
-     * fin si
-     */
-
-  }
 }
