785be4910ed589b4e2dd74e48b27ed2e96de4965
Antoine GIRARD <sapk@sapk.fr>
2016-12-01 14:59:07 +0100

    Exo 1 finish

--- 1_1-status.c
+++ 1_1-status.c
@@ -31,17 +31,20 @@ int main(int argc, char **argv) {
   struct stat *buffer;
   int r;
 
-  buffer = malloc(sizeof stat);
+  buffer = malloc(sizeof (struct stat));
   r = stat(argv[1], buffer);
   if (r < 0)
     erreur_grave("Stat");
 
   printf("Fichier %s:  mode: %X  Taille: %ld  Proprietaire: %u\n", argv[1], buffer->st_mode, buffer->st_size, buffer->st_uid);
   //*
-  //struct passwd *user;
+  struct passwd *user, *user2;
   //user = malloc(sizeof (struct passwd));
-  //user = getpwuid(1000);
-  printf("Fichier %s:  mode: %X  Taille: %ld  Proprietaire: %s\n", argv[1], buffer->st_mode, buffer->st_size, getpwuid(1000)->pw_name);
+  user = getpwuid(buffer->st_uid);
+  printf("Fichier %s:  mode: %X  Taille: %ld  Proprietaire: %s %d\n", argv[1], buffer->st_mode, buffer->st_size, user->pw_name, user);
+  
+  user2 = getpwuid(0);
+  printf("Proprietaire 1 : %s, Proprietaire 2 : %s %d %d\n", user->pw_name, user2->pw_name, user, user2);
   
   exit(EXIT_SUCCESS);
 }
